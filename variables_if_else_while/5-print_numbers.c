89c42a6d271cd54a2ce2cc21e5bb0a0854aa1d29
mehrajhuseynzadeh <mehrachuseynzade14@gmail.com>
2025-12-07 04:59:32 +0000

    task 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char c;
+        char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-		if (c != 'e' && c != 'q')
-			putchar(c);
-	putchar('\n');
+        for (c = 'a'; c <= 'z'; c++)
+                if (c != 'e' && c != 'q')
+                        putchar(c);
+        putchar('\n');
 
-	return (0);
+        return (0);
 }
