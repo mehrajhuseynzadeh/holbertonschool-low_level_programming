36ef4f7f1703beac9fd2b5885b0761b9c0fdc1aa
Your Name <you@example.com>
2025-12-08 18:21:11 +0000

    task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-    int n = 48; /* ASCII '0' */
+	int n = 48; /* ASCII '0' */
 
-    while (n <= 57) /* ASCII '9' */
-    {
-        putchar(n);
-        n++;
-    }
-    putchar('\n');
-    return (0);
+	while (n <= 57) /* ASCII '9' */
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
 }
