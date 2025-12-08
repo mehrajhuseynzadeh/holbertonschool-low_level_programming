0ef959c3eb31960973b4c60d45fa06a8d02a9007
Your Name <you@example.com>
2025-12-08 18:22:16 +0000

    task ^

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints single-digit numbers from 0 to 9 using putchar.
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int n = 48; /* ASCII '0' */
