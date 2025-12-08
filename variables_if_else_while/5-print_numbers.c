ac58cbb33dbb77d227567424cdade3f653d4d3bd
Your Name <you@example.com>
2025-12-08 07:03:04 +0000

    task 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d", i);
-    }
-    printf("\n");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
 
-    return (0);
+	return (0);
 }
