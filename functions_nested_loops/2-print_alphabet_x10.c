25b4cc4720b292ec71014b35e0a345f75799594c
Your Name <you@example.com>
2025-12-09 23:28:08 +0000

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+char *alphabet = "abcdefghijklmnopqrstuvwxyz\n";
+int i;
+for (i = 0; i < 10; i++)
+{
+int j = 0;
+while (alphabet[j] != '\0')
+{
+_putchar(alphabet[j]);
+j++;
+}
+}
+}
