720ed80ac758254324c36ac83c30c932c1f89b30
Your Name <you@example.com>
2025-12-10 12:21:53 +0000

    task 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-void reset_to_98(int *n)
-{
-    *n = 98;
-}
+int _putchar(char c);
+void reset_to_98(int *n);
+
+/* Buraya digər funksiyaların prototiplərini əlavə et */
+
+#endif /* MAIN_H */
