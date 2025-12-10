ec91e45f52a469e33b0694585b674e4973b8d89c
Your Name <you@example.com>
2025-12-10 12:42:35 +0000

    task 4

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void rev_string(char *s);
+void print_rev(char *s);
 
 /* Buraya digər funksiyaların prototiplərini əlavə et */
 
