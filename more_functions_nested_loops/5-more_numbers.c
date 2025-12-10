24365c5af72bc42b5d1c6474dc33f05279af63d5
Your Name <you@example.com>
2025-12-10 10:46:24 +0000

    task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 void more_numbers(void)
-{ 
-int i, n;
-for (i = 0; i < 10; i++)
 {
-for (n = 0; n <= 14; n++)
-{
-if (n > 9)
-_putchar('1');    /* birinci rəqəm (yalnız 1 olur) */
+    int i, n;
 
-_putchar((n % 10) + '0'); /* ikinci rəqəm */
-}
-_putchar('\n'); /* yeni sətir */
-}
+    for (i = 0; i < 10; i++)
+    {
+        for (n = 0; n <= 14; n++)
+        {
+            if (n > 9)
+                _putchar('1');
+            _putchar((n % 10) + '0');
+        }
+        _putchar('\n');
+    }
 }
