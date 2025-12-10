b081f6916c9e694f024982b942f761cb170ab3d4
Your Name <you@example.com>
2025-12-10 13:18:01 +0000

    task 100 atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: integer value
+ */
+int _atoi(char *s)
+{
+int i = 0, sign = 1, num = 0;
+
+/* skip non-digit characters and handle signs */
+while (s[i])
+{
+if (s[i] == '-')
+sign *= -1;
+else if (s[i] >= '0' && s[i] <= '9')
+      {
+/* start parsing number */
+while (s[i] >= '0' && s[i] <= '9')
+{
+num = num * 10 + (s[i] - '0');
+i++;
+}
+break;
+}
+i++;
+}
+return (num * sign);
+}
