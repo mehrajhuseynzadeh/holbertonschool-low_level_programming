ce804e20c45fac1d24c794374e9e64f0f3863f1c
Your Name <you@example.com>
2025-12-10 10:48:00 +0000

    task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,16 +2,16 @@
 
 void more_numbers(void)
 {
-    int i, n;
+int i, n;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (n = 0; n <= 14; n++)
-        {
-            if (n > 9)
-                _putchar('1');
-            _putchar((n % 10) + '0');
-        }
-        _putchar('\n');
-    }
+for (i = 0; i < 10; i++)
+{
+for (n = 0; n <= 14; n++)
+{
+if (n > 9)
+_putchar('1');
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
 }
