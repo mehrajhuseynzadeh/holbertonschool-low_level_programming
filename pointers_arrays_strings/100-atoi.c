e4b994523d60645729245f6c6be6d3fa8e77bc3a
Your Name <you@example.com>
2025-12-10 13:27:56 +0000

    task 100 atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,27 @@
 #include "main.h"
-
 /**
  * _atoi - converts a string to an integer
  * @s: string to convert
- *
  * Return: integer value
  */
 int _atoi(char *s)
 {
-int i = 0, sign = 1;
-unsigned int num = 0; /* unsigned to handle overflow safely */
-while (s[i])
+int i=0,sign=1;
+unsigned int num=0;
+while(s[i])
 {
-if (s[i] == '-')
-sign *= -1;
-else if (s[i] >= '0' && s[i] <= '9')
+if(s[i]=='-')
+sign*=-1;
+else if(s[i]>='0'&&s[i]<='9')
 {
-/* build number safely */
-while (s[i] >= '0' && s[i] <= '9')
+while(s[i]>='0'&&s[i]<='9')
 {
-if (num > (2147483647 - (s[i] - '0')) / 10)
-{
-/* handle overflow */
-return (sign == 1) ? 2147483647 : -2147483648;
-}
-num = num * 10 + (s[i] - '0');
+if(num>(2147483647-(s[i]-'0'))/10)
+return(sign==1?2147483647:-2147483648);
+num=num*10+(s[i]-'0');
 i++;
 }
 break;
 }
 i++;
-}
-
-return ((int)(num * sign));
 }
