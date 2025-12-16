eccb687ad70cf8be3340916379ac36ce0b66a102
Your Name <you@example.com>
2025-12-16 21:27:14 +0000

    task3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,7 +11,6 @@ unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i = 0, j;
 int match;
-
 while (s[i] != '\0')
 {
 match = 0;
@@ -29,6 +28,5 @@ if (!match)
 break;
 i++;
 }
-
 return (i);
 }
