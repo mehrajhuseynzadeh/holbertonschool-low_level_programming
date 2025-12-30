b6e11c55a85ad524bf575c4504f6c95c0f0ba8b0
Your Name <you@example.com>
2025-12-30 14:27:20 +0000

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
-#include <string.h>
 char *string_nconcat(char *s1,char *s2,unsigned int n)
 {
 char *concat;
-unsigned int len1,len2,total,i,j;
-if(s1==NULL)s1="";
-if(s2==NULL)s2="";
-len1=strlen(s1);
-len2=strlen(s2);
-if(n>=len2)n=len2;
+unsigned int len1=0,len2=0,total,i,j;
+if(!s1)s1="";
+if(!s2)s2="";
+while(s1[len1])len1++;
+while(s2[len2])len2++;
+if(n>len2)n=len2;
 total=len1+n;
 concat=malloc(total+1);
-if(concat==NULL)return(NULL);
+if(!concat)return NULL;
 for(i=0;i<len1;i++)concat[i]=s1[i];
 for(j=0;j<n;j++)concat[i+j]=s2[j];
 concat[i+j]='\0';
-return(concat);
+return concat;
 }
