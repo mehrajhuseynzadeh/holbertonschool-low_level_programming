e97593913d5f761ff22ad8754a07ed75ecc37ca3
Your Name <you@example.com>
2025-12-30 17:18:05 +0000

    task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,11 +1,18 @@
-#include "main.h"
-#include <stddef.h>
+#include "dog.h"
 
-void init_dog(struct dog *d,char *name,float age,char *owner)
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to the struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner's name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d==NULL)
-return;
-d->name=name;
-d->age=age;
-d->owner=owner;
+    if (d != NULL)
+    {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
 }
