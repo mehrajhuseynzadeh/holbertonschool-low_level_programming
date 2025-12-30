2f31c934cbc647237456efbe00ed94cfaa5d50d6
Your Name <you@example.com>
2025-12-30 13:31:52 +0000

    Task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -5,7 +5,8 @@
  * _strdup - returns a pointer to a newly allocated copy of a string
  * @str: string to duplicate
  *
- * Return: pointer to the duplicated string, or NULL if str is NULL or malloc fails
+ * Return: pointer to the duplicated string, or NULL if str is NULL
+ *         or malloc fails
  */
 char *_strdup(char *str)
 {
@@ -15,15 +16,15 @@ unsigned int i, len;
 if (str == NULL)
 return (NULL);
 
-/* find length of str */
-for (len = 0; str[len]; len++)
-;
+len = 0;
+while (str[len] != '\0')
+len++;
 
 dup = malloc(sizeof(char) * (len + 1));
 if (dup == NULL)
 return (NULL);
 
-for (i = 0; i <= len; i++) /* include null terminator */
+for (i = 0; i <= len; i++)
 dup[i] = str[i];
 
 return (dup);
