8ab421060e53090fd7b6f7d7417ff41423f3ee10
Your Name <you@example.com>
2025-12-30 14:23:51 +0000

    task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,16 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
  * malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
- *
- * Return: pointer to allocated memory, exit(98) if malloc fails
+ * @b: number of bytes
+ * Return: pointer to allocated memory, exits with 98 on failure
  */
 void *malloc_checked(unsigned int b)
 {
 void *ptr;
+
 ptr = malloc(b);
 if (ptr == NULL)
 exit(98);
-
 return (ptr);
 }
